fe0143cdd2cf5fdd31726b24d48b0e124872f0a1
KenshinRR <krreblando15@gmail.com>
2026-02-11 10:10:09 +0800

    Set up and loading of theme color

---
+++ JavaScripts/theme_loader.js
@@ -0,0 +1,14 @@
+// console.log("Fetching theme color");
+
+fetch('./JSON files/color_palete.json')
+  .then(response => response.json())
+  .then(data => {
+    // console.log("File found with primary color of " + data.primary_color);
+    // Apply JSON values as CSS variables
+    const root = document.documentElement;
+    root.style.setProperty('--primary_color', data.primary_color);
+    root.style.setProperty('--background_color', data.background_color);
+    root.style.setProperty('--text_color', data.text_color);
+    root.style.setProperty('--eye_catcher', data.eye_catcher);
+    root.style.setProperty('--compliment_eye_catcher', data.compliment_eye_catcher);
+  });
