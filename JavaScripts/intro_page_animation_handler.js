16bac2371131854e56b864cd73720a1eeddc787c
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-02-13 22:35:51 +0800

    intro page animation
    in the index.html, there is a text that goes to the middle and after reaching, goes to main_feed

---
+++ JavaScripts/intro_page_animation_handler.js
@@ -0,0 +1,7 @@
+const heading = document.getElementById("title_text");
+
+    // Listen for when the CSS animation ends
+    heading.addEventListener("animationend", () => {
+      // Redirect to another page
+      window.location.href = "../Pages/main_feed.html"; 
+    });
