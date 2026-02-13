a8947da429c15f03c2bce943684acd9035d85584
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-02-13 14:32:48 +0800

    Login button now disappears when a user has logged in already

--- JavaScripts/main_feed_buttons_handler.js
+++ JavaScripts/main_feed_buttons_handler.js
@@ -1,3 +1,12 @@
 // Login button
 const login_button = document.getElementById("login_button");
 login_button.addEventListener("click", () => window.location.href="/Pages/login_page.html");
+
+// Current user logged in
+var currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+// if empty check in the session log
+if (!currentUser) currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+
+// If user logs in already, hide the login button
+if (currentUser) login_button.style.display = "none";
