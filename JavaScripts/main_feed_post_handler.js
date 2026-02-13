35b41dd84aa02adf8a0cd1858ac382c599d4647a
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-02-13 22:49:53 +0800

    added interaction bar on main feed post
    not working

--- JavaScripts/main_feed_post_handler.js
+++ JavaScripts/main_feed_post_handler.js
@@ -5,11 +5,11 @@ function createNewPost(post_id, title, poster, desc)
     newPostDiv.dataset.post_id = post_id;
 
     var h1_title = document.createElement("h1");
-    newPostDiv.classList.add("clickable")
+    h1_title.classList.add("clickable")
     h1_title.textContent = title;
 
     var h2_title = document.createElement("h2");
-    newPostDiv.classList.add("clickable")
+    h2_title.classList.add("clickable")
     h2_title.textContent = poster
 
     var desc_p = document.createElement("p");
@@ -19,6 +19,8 @@ function createNewPost(post_id, title, poster, desc)
     newPostDiv.appendChild(h2_title);
     newPostDiv.appendChild(desc_p);
 
+    createInteractionBar(newPostDiv);
+
     document.getElementById("main_feed_container").appendChild(newPostDiv);
 }
 
@@ -26,3 +28,33 @@ var localPosts = JSON.parse(localStorage.getItem("posts"));
 localPosts.forEach(postData => {
   createNewPost(postData.post_id, postData.title, postData.poster, postData.description);
 });
+
+// Creating the post interaction bar
+function createInteractionBar(parent_elem)
+{
+  var newInteractionBar = document.createElement("div");
+  newInteractionBar.classList.add("post_interaction_bar");
+
+  var newLikesSpan = document.createElement("span");
+  newLikesSpan.id = "post_likes_display";
+  newLikesSpan.textContent = "Likes: 0";
+
+  var vote_buttons_div = document.createElement("div");
+  vote_buttons_div.classList.add("vote_buttons");
+
+  // Buttons
+  var new_post_like_button = document.createElement("button");
+  new_post_like_button.id = post_like_btn;
+  new_post_like_button.textContent = "Like";
+
+  var new_post_dilike_button = document.createElement("button");
+  new_post_dilike_button.id = post_dislike_btn;
+  new_post_dilike_button.textContent = "Disike";
+
+  vote_buttons_div.appendChild(new_post_like_button);
+  vote_buttons_div.appendChild(new_post_dilike_button);
+
+  parent_elem.appendChild(newInteractionBar);
+  parent_elem.appendChild(newLikesSpan);
+  parent_elem.appendChild(vote_buttons_div);
+}
