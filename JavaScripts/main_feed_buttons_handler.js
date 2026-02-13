1eabb96774d6eedefc7c88aad1cae2b35b0a8f41
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-02-13 14:45:54 +0800

    Added Profile Icon on top bar

--- JavaScripts/main_feed_buttons_handler.js
+++ JavaScripts/main_feed_buttons_handler.js
@@ -1,6 +1,10 @@
-// Login button
-const login_button = document.getElementById("login_button");
+// declaring the element variables
+const login_button = document.getElementById("login_button"); // Login button
+const profile_icon_button = document.getElementById("profile_icon_button");
+
+// Adding the events
 login_button.addEventListener("click", () => window.location.href="/Pages/login_page.html");
+profile_icon_button.addEventListener("click", () => window.location.href="/Pages/profile_view.html")
 
 // Current user logged in
 var currentUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -9,4 +13,13 @@ var currentUser = JSON.parse(localStorage.getItem("currentUser"));
 if (!currentUser) currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
 
 // If user logs in already, hide the login button
-if (currentUser) login_button.style.display = "none";
+if (currentUser) 
+{
+    login_button.style.display = "none";
+    profile_icon_button.style.display = "block";
+}
+else
+{
+    login_button.style.display = "block";
+    profile_icon_button.style.display = "none";
+}
