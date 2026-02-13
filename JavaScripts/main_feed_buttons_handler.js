e84aa642e3a59f7b4dbc97d6942bfd1becd5e413
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-02-13 16:22:46 +0800

    Added view and edit profile buttons and log out button on left bar

--- JavaScripts/main_feed_buttons_handler.js
+++ JavaScripts/main_feed_buttons_handler.js
@@ -1,10 +1,19 @@
 // declaring the element variables
 const login_button = document.getElementById("login_button"); // Login button
 const profile_icon_button = document.getElementById("profile_icon_button");
+const nav_button = document.getElementById("navigation_button");
+const left_bar = document.getElementById("left_bar_container");
+const logout_button = document.getElementById("logout_button");
+const left_bar_view_profile = document.getElementById("left_bar_view_profile");
+const left_bar_edit_profile = document.getElementById("left_bar_edit_profile");
 
 // Adding the events
 login_button.addEventListener("click", () => window.location.href="/Pages/login_page.html");
-profile_icon_button.addEventListener("click", () => window.location.href="/Pages/profile_view.html")
+profile_icon_button.addEventListener("click", ViewProfilePage);
+left_bar_view_profile.addEventListener("click", ViewProfilePage);
+left_bar_edit_profile.addEventListener("click", () => window.location.href="/Pages/profile_edit.html");
+nav_button.addEventListener("click", ToggleLeftBar);
+logout_button.addEventListener("click", OnLogOutUser);
 
 // Current user logged in
 var currentUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -22,4 +31,37 @@ else
 {
     login_button.style.display = "block";
     profile_icon_button.style.display = "none";
+}
+
+// Toggling of left bar
+var left_bar_toggled = false;
+ToggleLeftBar();
+
+function ToggleLeftBar()
+{
+    if (left_bar_toggled)
+    {
+        left_bar.style.display = "block";
+    }
+    else
+    {
+        left_bar.style.display = "none";
+    }
+
+    left_bar_toggled = !left_bar_toggled;
+}
+
+// On log out
+function OnLogOutUser()
+{
+    localStorage.removeItem("currentUser");
+    sessionStorage.removeItem("currentUser");
+
+    window.location.href="/Pages/login_page.html";
+}
+
+// ON viewing profile page
+function ViewProfilePage()
+{
+    window.location.href="/Pages/profile_view.html"
 }
