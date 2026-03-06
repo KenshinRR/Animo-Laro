88ce6af09e3c28f66a0bdd79ee37fa7ec721887a
railey13 <nikosrailey@gmail.com>
2026-03-06 21:08:19 +0800

    commented out duplicate back button

--- JavaScripts/view_specific_post_handler.js
+++ JavaScripts/view_specific_post_handler.js
@@ -38,5 +38,5 @@ else{
 }
 
 // Back button
-const back_button = document.getElementById("back_button");
-back_button.addEventListener("click", () => window.location.href="/Pages/main_feed.html")
+// const back_button = document.getElementById("back_button");
+// back_button.addEventListener("click", () => window.location.href="/Pages/main_feed.html")
