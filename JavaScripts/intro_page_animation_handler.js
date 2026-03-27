a58912a06690fa756a6b2263ba0cc6c5a9ba42d9
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-03-27 12:17:50 +0800

    Using absolute paths

--- JavaScripts/intro_page_animation_handler.js
+++ JavaScripts/intro_page_animation_handler.js
@@ -3,5 +3,5 @@ const heading = document.getElementById("title_text");
 // Listen for when the CSS animation ends
 heading.addEventListener("animationend", () => {
   // Redirect to another page
-  window.location.href = "../Pages/main_feed.html"; 
+  window.location.href = "/Animo-Laro/Pages/main_feed.html";
 });
