6514845137f808251451fd3c188c3d2dc53c5181
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-03-27 17:30:45 +0800

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,15 @@
 export function requireAuth(req, res, next) {
     if (!req.session.user) {
+        console.log("No session user found!");
         return res.status(401).json({ error: "Unauthorized" });
     }
 
     const token = req.cookies.token;
-    if (!token) return res.status(401).json({ error: "Unauthorized" });
+    if (!token)
+    {
+        console.log("No Token found!");
+        return res.status(401).json({ error: "Unauthorized" });
+    }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -13,6 +18,6 @@ export function requireAuth(req, res, next) {
     } catch (err) {
         return res.status(401).json({ error: "Invalid token" });
     }
-    
+
     next();
 }
