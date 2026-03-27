6da7e77b808f086ac44322a014fcec5b95d2a6c4
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-03-27 16:48:00 +0800

    Added Origin to error output debug

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(cors({
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS \nOrigin: " + origin));
     }
   },
   credentials: true
