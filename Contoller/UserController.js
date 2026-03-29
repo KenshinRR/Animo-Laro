8661f9dc240023e3f63699dbdd7b420ee56a7223
railey13 <nikosrailey@gmail.com>
2026-03-29 22:30:23 +0800

    removed token

--- Contoller/UserController.js
+++ Contoller/UserController.js
@@ -69,15 +69,6 @@ export async function loginUser(req, res) {
             bio:user.bio,
             avatar:user.avatar
         };
-
-        // validate user credentials...
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-
-        res.cookie("token", token, {
-            httpOnly: true,       // cannot be accessed by JS
-            secure: true,         // only sent over HTTPS
-            sameSite: "strict"    // prevents CSRF
-        });
         
         console.log("Login successful!");
         res.json({ message: 'Login successful!', user });
