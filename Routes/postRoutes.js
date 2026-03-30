108446c1fa5eab51f39b3bcbff4210fae1b6e563
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-03-31 01:57:30 +0800

    Added requireAuth to postRoutes

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as postController from "../Contoller/PostController.js";
+import { requireAuth } from "../middleware/auth.js";
 // import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -12,8 +13,8 @@ const router = express.Router();
 
 router.get("/posts", postController.getPosts);
 router.get("/posts/:id", postController.getSpecificPost);
-router.post("/create_post", postController.createPost);
-router.post("/edit_post/:id", postController.editPost);
-router.post("/delete_post/:id", postController.deletePostByID);
+router.post("/create_post", requireAuth, postController.createPost);
+router.post("/edit_post/:id", requireAuth, postController.editPost);
+router.post("/delete_post/:id", requireAuth, postController.deletePostByID);
 
 export default router;
