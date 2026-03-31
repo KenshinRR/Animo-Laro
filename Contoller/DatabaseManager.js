3ae1e5b343353cd880cd73928c7b1e52b9fe7b91
KenshinRR <156569601+KenshinRR@users.noreply.github.com>
2026-03-31 17:47:00 +0800

    putting back what was lost regarding posts on DatabaseManager

--- Contoller/DatabaseManager.js
+++ Contoller/DatabaseManager.js
@@ -17,6 +17,71 @@ class DatabaseManager {
     return posts;
   }
 
+  async addPost(post){
+    var title = post.title;
+    var poster_name = post.poster_name;
+    var poster_id = post.poster_id;
+    var description = post.description;
+    var likes = post.likes;
+    var link = post.link;
+    
+    fetch('https://animo-laro.onrender.com/api/create_post',{
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        credentials: "include",
+        body: JSON.stringify({title, poster_name, poster_id, description, likes, link})
+    })
+    .then(response => {
+      response.json()
+      if (!response.ok)
+      {
+        alert("Post failed to add!");
+      }
+    })
+    .catch(err => {
+      console.error("Error:", err);
+    });
+  }
+
+  async editPost(id, post) {
+    var title = post.title;
+    var description = post.description;
+    var link = post.link;
+        await fetch('https://animo-laro.onrender.com/api/edit_post/' + id,{
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({"id": id, "post_data": {title, description, link}})
+    })
+    .then(response => {
+      response.json()
+      if (!response.ok)
+      {
+        alert("Post failed to edit!");
+      }
+    })
+    .catch(err => {
+      console.error("Error:", err);
+    });
+  }
+
+  async deletePostByID(_id) {
+    fetch('https://animo-laro.onrender.com/api/delete_post/' + _id,{
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({id: _id})
+      })
+      .then(response => {
+        response.json()
+        if (!response.ok)
+        {
+          alert("Post failed to add!");
+        }
+      })
+      .catch(err => {
+        console.error("Error:", err);
+      });
+    }
+
   async getCurrentUser() {
     let curr_user = null;
     try {
